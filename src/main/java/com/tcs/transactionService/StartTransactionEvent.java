c6278722b4b0e8ce2b16604fab95b66db8aad01c
Justin Chan <justin.chan.3719@gmail.com>
2023-12-02 11:48:09 -0500

    Simple code base[entity, repository, controller]

--- src/main/java/com/tcs/transactionService/StartTransactionEvent.java
+++
@@ -1,9 +0,0 @@
-package com.tcs.transactionService;
-
-import org.springframework.context.ApplicationEvent;
-
-public class StartTransactionEvent extends ApplicationEvent {
-	public StartTransactionEvent(Object source) {
-		super(source);
-	}
-}
--- src/main/java/com/tcs/transactionService/TransactionController.java
+++ src/main/java/com/tcs/transactionService/TransactionController.java
@@ -1,8 +1,6 @@
 package com.tcs.transactionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionController {
 	@Autowired
 	private TransactionRepository transactionRepository;
-	@Autowired
-	private EventPublisher eventPublisher;
 
-	@PostMapping("/initiate")
-	public Transaction initiateTransaction(@RequestBody Transaction transaction) {
-		transaction.setStatus(Status.PENDING);
-		transactionRepository.save(transaction);
-		eventPublisher.publishStartTransactionEvent(new StartTransactionEvent(transaction.getId()));
-		return transaction;
-	}
 }
